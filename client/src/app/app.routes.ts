fa02f6139faff4f8187fab011078362258a1fae9
IMMihaylov <try.mihaylov@gmail.com>
2026-05-31 23:25:00 +0200

    feat: update app layout and routing structure for improved navigation

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 import { EmployeesList } from './employees-list/employees-list';
 import { AddEmployee } from './add-employee/add-employee';
 import { EditEmployeeComponent } from './edit-employee-component/edit-employee-component';
+import { Dashboard } from './dashboard/dashboard';
+import { Todos } from './todosStore/todos/todos';
 
 export const routes: Routes = [
-    {path: '', component: EmployeesList, title: 'Employees List'},
-    {path: 'new', component: AddEmployee, title: 'Add Employee'},
-    {path: 'edit/:id', component: EditEmployeeComponent, title: 'Edit Employee'},
+    {path: '', component: Dashboard, title: 'Dashboard'},
+    {path: 'employees', component: EmployeesList, title: 'Employees List'},
+    {path: 'todos', component: Todos, title: 'Todos'},
+    {path: 'employees/new', component: AddEmployee, title: 'Add Employee'},
+    {path: 'employees/edit/:id', component: EditEmployeeComponent, title: 'Edit Employee'},
+    {path: 'employee/new', component: AddEmployee, title: 'Add Employee'},
+    {path: 'employee/edit/:id', component: EditEmployeeComponent, title: 'Edit Employee'},
 ];
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,15 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { EmployeesList } from './employees-list/employees-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EmployeesList, MatToolbarModule, MatButtonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
-}
+export class App {}
